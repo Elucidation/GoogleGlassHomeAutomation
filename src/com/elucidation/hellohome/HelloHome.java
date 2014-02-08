c7964a889d65097da52b4af922be089ebbcba30e
Sameer Ansari <elucidation@gmail.com>
2014-02-08 00:57:19 -0500

    Adding a static card after 10 seconds, removing it after 10 more

--- src/com/elucidation/hellohome/HelloHome.java
+++ src/com/elucidation/hellohome/HelloHome.java
@@ -10,9 +10,11 @@ import com.google.android.glass.timeline.TimelineManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.util.Log;
+import android.util.Log; 
 
 public class HelloHome extends Service {
+	private static final String LOG_TAG = "SAM_DEBUG";
+
 	/*
 	 * TimelineManager allows applications to interact with the timeline.
 	 * 
@@ -28,6 +30,12 @@ public class HelloHome extends Service {
 		timeline = TimelineManager.from(this);
 	}
 	
+	@Override
+	public void onDestroy() {
+		Log.v(LOG_TAG, "Closing down Home!");
+		super.onDestroy();
+	}
+	
 	
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -40,12 +48,31 @@ public class HelloHome extends Service {
 	 * @see android.app.Service#onStartCommand(android.content.Intent, int, int)
 	 */
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.v("SAM_DEBUG", "We're starting home!!");
-		Card card = new Card(this);
-		card.setText("Hello Home");
+		Log.v(LOG_TAG, "We're starting home!!");
 		
-		// Add card to timeline
-		timeline.insert(card);
+		long cardId;
+		Card card = new Card(this);
+		card.setText("Hello Home Again");
+		
+		synchronized (this) {
+			try {
+				Log.v(LOG_TAG, "Waiting");
+				wait(10000);
+				Log.v(LOG_TAG, "Adding Card.");
+				// Add card to timeline
+				cardId = timeline.insert(card);
+				wait(10000);
+				Log.v(LOG_TAG, "Removing Card.");
+				timeline.delete(cardId);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		Log.v(LOG_TAG, "Finishing.");
+		
+		this.stopSelf();
 		
 		return startId;
 	}
