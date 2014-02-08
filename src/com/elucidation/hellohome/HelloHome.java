b52a09e1a285109655de6e16e5e33b71b957a769
Sameer Ansari <elucidation@gmail.com>
2014-02-08 01:44:07 -0500

    activity only, has voice result in local variable

--- src/com/elucidation/hellohome/HelloHome.java
+++ src/com/elucidation/hellohome/HelloHome.java
@@ -6,33 +6,24 @@ package com.elucidation.hellohome;
 
 import java.util.ArrayList;
 
-import com.google.android.glass.app.Card;
-import com.google.android.glass.timeline.TimelineManager;
-
-import android.app.Service;
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.speech.RecognizerIntent;
 import android.util.Log; 
 
-public class HelloHome extends Service {
-	private static final String LOG_TAG = "SAM_DEBUG";
+public class HelloHome extends Activity {
 
-	/*
-	 * TimelineManager allows applications to interact with the timeline.
-	 * 
-	 * Additional information: https://developers.google.com/glass/develop/gdk/reference/com/google/android/glass/timeline/TimelineManager
-	 */
-	private TimelineManager timeline;
+	private static final String LOG_TAG = "SAM_DEBUG";
 	
 	
 	@Override
-	public void onCreate() {
-		super.onCreate();
-		// Start timeline manager from context
-		timeline = TimelineManager.from(this);
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.v(LOG_TAG, "Starting up Home!");
+		super.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		Log.v(LOG_TAG, "Closing down Home!");
@@ -40,51 +31,25 @@ public class HelloHome extends Service {
 	}
 	
 	
-	@Override
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
 	
-	/*
-	 * onStartCommand is used to start a service from the voice trigger in res/xml/voice_trigger_start.xml
-	 * (non-Javadoc)
-	 * @see android.app.Service#onStartCommand(android.content.Intent, int, int)
-	 */
-	public int onStartCommand(Intent intent, int flags, int startId) {
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
 		Log.v(LOG_TAG, "We're starting home!!");
-		ArrayList<String> voiceResults = intent.getExtras()
-	            .getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
+		ArrayList<String> voiceResults = getIntent().getExtras()
+		        .getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
 		
 		// Hack to '[example, string, message]' to 'example string message'
 		String voiceMessage = voiceResults.toString().replace(", ", " ").replaceAll("[\\[\\]]", "");
 		
 		Log.v(LOG_TAG, String.format("Received Voice Command: %s", voiceMessage));
 		
-		long cardId;
-		Card card = new Card(this);
-		card.setText(String.format("Hello Home heard you say %s", voiceMessage));
 		
-		synchronized (this) {
-			try {
-				Log.v(LOG_TAG, "Waiting");
-				wait(1000);
-				Log.v(LOG_TAG, "Adding Card.");
-				// Add card to timeline
-				cardId = timeline.insert(card);
-				wait(10000);
-				Log.v(LOG_TAG, "Removing Card.");
-				timeline.delete(cardId);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		
 		Log.v(LOG_TAG, "Finishing.");
-		
-		this.stopSelf();
-		
-		return startId;
 	}
-
 }
