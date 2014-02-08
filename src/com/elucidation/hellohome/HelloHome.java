0c48381477916bcf0db9211b2eb5a131c8a286b5
Sameer Ansari <elucidation@gmail.com>
2014-02-08 04:22:51 -0500

    working temperature info, turning lights on & off

--- src/com/elucidation/hellohome/HelloHome.java
+++ src/com/elucidation/hellohome/HelloHome.java
@@ -30,7 +30,7 @@ public class HelloHome extends Activity {
 	private static final String LOG_TAG = "SAM_DEBUG";
 	
 	// URL for querying info (on my local network for example)
-	private static final String URL = "<EXAMPLE-REST-SERVICE>";
+	private static final String URL = "http://192.168.62.177/";
 
 	// Request result
 	private String info_result;
@@ -75,7 +75,7 @@ public class HelloHome extends Activity {
 		setContentView(card.toView());
 		
 		// If the message is "get info"
-		if (voiceMessage.equalsIgnoreCase("get info")) {
+		if (voiceMessage.contains("temperature")) {
 			Log.v(LOG_TAG, String.format("Getting info!"));
 			
 	        
@@ -86,13 +86,37 @@ public class HelloHome extends Activity {
 			setContentView(info_card.toView());
 			
 			// Call web service (which updates info_card
-			new queryWebService().execute();
+			new queryWebService().execute("info");
+		}
+		else if (voiceMessage.contains("turn") && voiceMessage.contains("light") && voiceMessage.contains("on")) {
+			Log.v(LOG_TAG, String.format("Setting LED On!"));
+			
+	        // Create a card showing what web service returned
+			info_card = new Card(this);
+			info_card.setText("");
+			info_card.setFootnote("Asking web service to turn on LED...");
+			setContentView(info_card.toView());
+			
+			// Call web service (which updates info_card
+			new queryWebService().execute("led", "on");
+		}
+		else if (voiceMessage.contains("turn") && voiceMessage.contains("light") && voiceMessage.contains("off")) {
+			Log.v(LOG_TAG, String.format("Setting LED Off!"));
+			
+	        // Create a card showing what web service returned
+			info_card = new Card(this);
+			info_card.setText("");
+			info_card.setFootnote("Asking web service to turn off LED...");
+			setContentView(info_card.toView());
+			
+			// Call web service (which updates info_card
+			new queryWebService().execute("led", "off");
 		}
 		
 		Log.v(LOG_TAG, "Finishing.");
 	}
 	
-	private class queryWebService extends AsyncTask<Void, Void, String> {
+	private class queryWebService extends AsyncTask<String, Void, String> {
 		@Override
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
@@ -111,25 +135,53 @@ public class HelloHome extends Activity {
 		protected void onProgressUpdate(Void... values) {}
 
 		@Override
-		protected String doInBackground(Void... params) {
+		protected String doInBackground(String... params) {
+			
 			HttpClient httpclient = new DefaultHttpClient();
 			HttpGet request = new HttpGet(URL);
 			request.addHeader("applicationName", "Glass Hello Home");
 			
 			ResponseHandler<String> handler = new BasicResponseHandler();
 			
-			try {
-				info_result = httpclient.execute(request, handler);
-			} catch (ClientProtocolException e) {  
-	            e.printStackTrace();  
-	        } catch (IOException e) {  
-	            e.printStackTrace();  
-	        }  
-	        httpclient.getConnectionManager().shutdown();
-	        if (info_result == null) {
-	        	info_result = "Couldn't connect :(";
-	        }
-	        Log.i(LOG_TAG, info_result);
+			if (params.length > 0 && params[0].equalsIgnoreCase("info")) {
+				request.addHeader("command", "info");
+				try {
+					info_result = httpclient.execute(request, handler);
+				} catch (ClientProtocolException e) {  
+		            e.printStackTrace();  
+		        } catch (IOException e) {  
+		            e.printStackTrace();  
+		        }  
+		        httpclient.getConnectionManager().shutdown();
+		        if (info_result == null) {
+		        	info_result = "Couldn't connect :(";
+		        }
+		        Log.i(LOG_TAG, info_result);
+			}
+			else if (params.length > 0 && params[0].equalsIgnoreCase("led")) {
+				if (params.length == 2) {
+					if (params[1].equalsIgnoreCase("on"))
+						request.addHeader("command", "led on");
+					else
+						request.addHeader("command", "led off");
+					
+				} else {
+					Log.w(LOG_TAG, "Error with passed params, ignoring.");
+				}
+				try {
+					info_result = httpclient.execute(request, handler);
+				} catch (ClientProtocolException e) {  
+		            e.printStackTrace();  
+		        } catch (IOException e) {  
+		            e.printStackTrace();  
+		        }  
+		        httpclient.getConnectionManager().shutdown();
+		        if (info_result == null) {
+		        	info_result = "Couldn't connect :(";
+		        }
+		        Log.i(LOG_TAG, info_result);
+			}
+			
 			return "Execution Finished";
 		}
 		
